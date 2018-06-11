5333dd103e739b58a7720d381fb253ef643bdcf3
Dima Derevyanko <derevyanko977@gmail.com>
2018-06-11 12:08:42 +0300

    add GEO templates 6.1, 6.2 in one page without JS

--- protected/views/frontend/user/projects/item.php
+++ protected/views/frontend/user/projects/item.php
@@ -29,7 +29,7 @@ echo "</pre>";
 				<b>ГЕОЛОКАЦИЯ</b>
 			</a>
 			<a href="?s=route" class="<?=$s=='route'?'active':''?>">
-				<b>МАРШРУТ</b>
+				<b>МАРШРУТ ГЕО</b>
 			</a>
 			<a href="?s=tasks" class="<?=$s=='tasks'?'active':''?>">
 				<b>ЗАДАНИЯ</b>
@@ -269,17 +269,19 @@ echo "</pre>";
 						<input type="file" name="xls" class="hide" accept="xls">
 					</div>
 					<div class="project__addr-filter">
-						<div>
+						<div class="addr__header-city">
 							<label>Город</label>
 							<input type="text" name="city">
 						</div>
-						<div>
-							<label>Дата с</label>
-							<input type="text" name="bdate" class="addr__filter-date">
-						</div>
-						<div>
-							<label>По</label>
-							<input type="text" name="edate" class="addr__filter-date">
+						<div class="addr__header-date">
+							<div>
+								<label>Дата с</label>
+								<input type="text" name="bdate" class="addr__filter-date">
+							</div>
+							<div>
+								<label>По</label>
+								<input type="text" name="edate" class="addr__filter-date">
+							</div>
 						</div>
 					</div>
 				</div>
@@ -553,7 +555,126 @@ echo "</pre>";
 			//
 			?>
 			<?php elseif($s=='route'): ?>
-			<div class="project__module">Макет 6.1 и 6.2</div>
+			<div class="project__module">
+				<div class="project__route-header">
+					<div class="project__addr-xls">
+						<a href="#">Изменить адресную программу</a>
+						<a href="#">Скачать существующую</a>
+						<a href="#">Добавить адресную программу</a>
+						<input type="file" name="xls" class="hide" accept="xls">
+					</div>
+					<div class="project__route-filter">
+						<div class="route__header-city">
+							<label>Город</label>
+							<input type="text" name="city">
+						</div>
+						<div class="route__header-date">
+							<div>
+								<label>Дата с</label>
+								<input type="text" name="bdate" class="route__filter-date">
+							</div>
+							<div>
+								<label>По</label>
+								<input type="text" name="edate" class="route__filter-date">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="routes">
+					<div class="route__item">
+						<h2 class="route__item-title">Харьков</h2>
+						<table class="route__table">
+							<thead>
+								<tr>
+									<th>ФИО</th>
+									<th>Название ТТ</th>
+									<th>Адрес ТТ</th>
+									<th>Статус посещения</th>
+									<th>Дата</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td rowspan="3">
+										<div class="route__table-cell route__table-user">
+											<img src="/images/applic/20180503073112204100.jpg">
+											<span>Дмитриев<br/>Николай</span>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border">АТБ1</div>
+									</td>
+									<td>
+										<div class="route__table-cell border route__table-index">
+											<span>ул. Пирогова 23</span>
+											<b class="js-g-hashint" title="Посмотреть на карте"></b>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border route__table-status">
+											<span>2</span>
+											<a href="#">изменить</a>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border text-center">14.02.2018</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="route__table-cell border">ВАРУС</div>
+									</td>
+									<td>
+										<div class="route__table-cell border route__table-index">
+											<span>пр. Кирова 18</span>
+											<b class="js-g-hashint" title="Посмотреть на карте"></b>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border route__table-status">
+											<span>1</span>
+											<a href="#">изменить</a>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border text-center">14.02.2018</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="route__table-cell border">СЕЛЬПО</div>
+									</td>
+									<td>
+										<div class="route__table-cell border route__table-index">
+											<span>ул. Строителей 4</span>
+											<b class="js-g-hashint" title="Посмотреть на карте"></b>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border route__table-status">
+											<span>3</span>
+											<a href="#">изменить</a>
+										</div>
+									</td>
+									<td>
+										<div class="route__table-cell border text-center">14.02.2018</div>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="routes__map">
+					<div class="routes__map-city">Харьков</div>
+					<div class="routes__map-map">
+						<img src="/theme/pic/projects/temp-map-2.jpg">
+					</div>
+				</div>
+				<div class="routes__btns">
+					<a href="#" class="route__watch-btn">ИЗМЕНИТЬ</a>
+					<a href="#" class="route__watch-btn">СМОТРЕТЬ МАРШРУТ</a>
+				</div>
+			</div>
 			<?php 
 			//
 			//
