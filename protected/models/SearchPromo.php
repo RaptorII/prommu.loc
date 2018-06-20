8896ff8741df5042be33c48c32abcc87cad0c9ee
greskode <denisgresk@gmail.com>
2018-06-20 10:06:58 +0000

    SearchPromo.php исправлена ошибка поиска по телефону

--- protected/models/SearchPromo.php
+++ protected/models/SearchPromo.php
@@ -82,7 +82,7 @@ class SearchPromo extends Model
                 INNER JOIN user_city uc ON r.id_user = uc.id_user  AND !(uc.id_city IN({$strCities}))
                 {$filter['table']}
                 INNER JOIN user_mech a ON a.id_us = r.id_user
-                INNER JOIN user_attribs ua ON ua.id_us = r.id_user
+                LEFT JOIN user_attribs ua ON r.id_user = ua.id_us
                 {$filter['filter']}
                 ORDER BY r.id DESC ";
         /** @var $res CDbCommand */
