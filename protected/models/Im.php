d34d63b53633566b82f88ac6a2b937e924ab5c5d
greskode <denisgresk@gmail.com>
2018-08-02 10:26:23 +0000

    Im.php added accessMessage method

--- protected/models/Im.php
+++ protected/models/Im.php
@@ -21,4 +21,39 @@ abstract class Im extends Model
 
         $this->Profile = $Profile ?: Share::$UserProfile;
     }
+
+    public function accessMessage($idto){
+        if(Share::$UserProfile->type == 3){
+            $result['user'] = Yii::app()->db->createCommand()
+            ->select("u.ismoder,u.isblocked")
+            ->from('user u')
+            ->where('u.id_user=:st', array(':st'=>Share::$UserProfile->id))
+            ->queryRow();
+
+            if($idto){
+                $result['new'] = Yii::app()->db->createCommand()
+                ->select("r.id")
+                ->from('resume r')
+                ->where('r.id=:st', array(':st'=>$idto))
+                ->queryRow();
+            }   
+        } else {
+            $result['user'] = Yii::app()->db->createCommand()
+            ->select("u.ismoder,u.isblocked")
+            ->from('user u')
+            ->where('u.id_user=:st', array(':st'=>Share::$UserProfile->id))
+            ->queryRow();
+
+            if($idto){
+                $result['new'] = Yii::app()->db->createCommand()
+                ->select("e.id")
+                ->from('employer e')
+                ->where('e.id=:st', array(':st'=>$idto))
+                ->queryRow();
+            }
+            
+        }
+
+        return $result;
+    }
 }
