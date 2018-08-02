734d5c740a1503cfa4caedec931010ea85b6a4ae
Dima Derevyanko <derevyanko977@gmail.com>
2018-08-02 16:49:37 +0300

    fix adress fields

--- theme/js/projects/new.js
+++ theme/js/projects/new.js
@@ -46,7 +46,7 @@ var ProjectPage = (function () {
     };
     //      Проверка готовности для создания проекта
     ProjectPage.prototype.checkErrors = function () {
-        //$('#new-project').submit(); //dump
+        $('#new-project').submit(); //dump
         console.log($('#new-project').serializeArray());
         let nameInp = $('#project-name'),
             name = $(nameInp).val();
@@ -1020,16 +1020,9 @@ var ProjectAddIndexProg = (function () {
     ProjectAddIndexProg.prototype.saveProgram = function (e) {
         let self = this;
 
-        let arr = $('#new-project').serializeArray(),
-            str = '';
-
-        for (var i=0; i < arr.length; i++) {
-           str += arr[i]['name'] + ' : ' + arr[i]['value'] + '\n';
-        }
-        console.log(str);
-        /*!self.checkFields()
+        !self.checkFields()
         ? self.Project.showModule(e)
-        : MainProject.showPopup('notif','save-program');*/
+        : MainProject.showPopup('notif','save-program');
     }
     //    получаем уникальные ID
     ProjectAddIndexProg.prototype.getNewId = function () {
