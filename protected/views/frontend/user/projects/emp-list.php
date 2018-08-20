7ee40413e976c6ec030a4ef72d410b98acd2cb02
Den <den@locahost>
2018-08-20 05:41:17 -0500

    Update emp-list.php

--- protected/views/frontend/user/projects/emp-list.php
+++ protected/views/frontend/user/projects/emp-list.php
@@ -3,15 +3,20 @@
     $this->setBreadcrumbs($title, MainConfig::$PAGE_PROJECT_LIST);
     $this->setPageTitle($title);
 	$bUrl = Yii::app()->baseUrl;
+	$data['projects'] = array(
+    0 => array(
+      'id' => 2323));
+
+	$projects = $data['projects'];
 	Yii::app()->getClientScript()->registerCssFile($bUrl . '/theme/css/projects/emp-list.css');
 ?>
 <div class="row projects">
 	<div class="col-xs-12">
 		<h1 class="projects__title">ВЫБЕРИТЕ ПРОЕКТ</h1>
 		<div class="projects__list">
-			<a href="/user/projects/1" class="projects__item">ПРОЕКТ 1</a>
-			<a href="/user/projects/1" class="projects__item">ПРОЕКТ 2</a>
-			<a href="/user/projects/1" class="projects__item">ПРОЕКТ 3</a>
+		    <? for($i = 0; $i < count($projects); $i ++):?>
+			<a href="/user/projects/<?=$projects['id']?>" class="projects__item">ПРОЕКТ 1</a>
+			<? endfor;?>
 		</div>
 		<div class="projects__btn">
 			<a href="<?=MainConfig::$PAGE_PROJECT_NEW?>">+ Добавить проект</a>
