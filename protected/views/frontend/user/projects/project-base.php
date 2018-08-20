9d832a6e8bea358686299639559799174e2ddbb1
Den <den@locahost>
2018-08-20 05:59:40 -0500

    Update project-base.php

--- protected/views/frontend/user/projects/project-base.php
+++ protected/views/frontend/user/projects/project-base.php
@@ -102,74 +102,7 @@ echo "</pre>";
 						<?
 						//
 						?>
-						<tr class="program__item">
-							<td colspan="4">
-								<div class="program__city border">РОСТОВ</div>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<div class="program__cell green-name border">АТБ1</div>
-							</td>
-							<td>
-								<div class="program__cell border">ул. Пирогова 23</div>
-							</td>
-							<td>
-								<div class="program__cell border user">
-									<div class="program__cell-user">
-										<img src="/theme/pic/projects/user-logo.png">
-										<span>Александр Примак</span>
-									</div>
-									<a href="#"><span>Изменить</span></a>
-								</div>
-							</td>
-							<td>
-								<div class="program__cell program__cell-period">
-									<span>06.02.2018 до 07.02.2018</span>
-									<span class="program__cell-tiem">12:00 - 14:00</span>
-									<a href="#"><span>Изменить</span></a>
-								</div>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<div class="program__cell green-name border">АТБ1</div>
-							</td>
-							<td>
-								<div class="program__cell border">ул. Паторжинского 23/4</div>
-							</td>
-							<td>
-								<div class="program__cell border user">
-									<a href="#"><span>Выбрать персонал </span><b>&#9660</b></a>
-								</div>
-							</td>
-							<td>
-								<div class="program__cell program__form">
-									<div class="program__input">
-										<input type="text" name="bdate[]" placeholder="Дата начала работ">
-									</div>
-									<div class="program__input">
-										<input type="text" name="edate[]" placeholder="Дата окончания работ">
-									</div>
-									<div class="program__time">
-										<div class="program__input">
-											<input type="text" name="btime[]" placeholder="Время с">
-										</div>
-										<div class="program__input">
-											<input type="text" name="etime[]" placeholder="Время до">
-										</div>
-									</div>
-								</div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="4">
-								<div class="program__btns">
-									<a href="#" class="program__add-btn">+ ДОБАВИТЬ ПЕРИОД</a>
-									<a href="#" class="program__save-btn">СОХРАНИТЬ</a>
-								</div>
-							</td>
-						</tr>
+			
 					</tbody>
 				</table>
 			</div>
