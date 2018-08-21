ce15254086c8abf8a15619de136d0e9f2eeedda9
Stanislav Kuzovov <s08002529@gmail.com>
2018-08-21 13:57:47 +0300

    Тест

--- protected/views/frontend/user/projects/project-staff.php
+++ protected/views/frontend/user/projects/project-staff.php
@@ -2,39 +2,36 @@
 Yii::app()->getClientScript()->registerCssFile($bUrl . '/theme/css/projects/item.css');
 Yii::app()->getClientScript()->registerScriptFile($bUrl . '/theme/js/projects/item.js', CClientScript::POS_END);
 $arPromo = array(
-                0 =>array(
-                    'image'=>'/images/applic/20180503073112204100.jpg', 
-                    'ttlink'=>'',
-                    'name'=>'Denis Gresko',
-                    'city'=>'Moskow'
-                ), 
-                1 =>array(
-                    'image'=>'/images/applic/20180503073112204100.jpg', 
-                    'ttlink'=>'',
-                    'name'=>'Denis Gresko',
-                    'city'=>'Moskow'
-                ), 
-                2 =>array(
-                    'image'=>'/images/applic/20180428142455264100.jpg', 
-                    'ttlink'=>'',
-                    'name'=>'Sasha Meet',
-                    'city'=>'Moskow'
-                ), 
-                3 =>array(
-                    'image'=>'/images/applic/20180428142455264100.jpg', 
-                    'ttlink'=>'',
-                    'name'=>'Sasha Meet',
-                    'city'=>'Moskow'
-                )
-            );
-
+    0 =>array(
+        'image'=>'/images/applic/20180503073112204100.jpg',
+        'ttlink'=>'',
+        'name'=>'Denis Gresko',
+        'city'=>'Moskow'
+    ),
+    1 =>array(
+        'image'=>'/images/applic/20180503073112204100.jpg',
+        'ttlink'=>'',
+        'name'=>'Denis Gresko',
+        'city'=>'Moskow'
+    ),
+    2 =>array(
+        'image'=>'/images/applic/20180428142455264100.jpg',
+        'ttlink'=>'',
+        'name'=>'Sasha Meet',
+        'city'=>'Moskow'
+    ),
+    3 =>array(
+        'image'=>'/images/applic/20180428142455264100.jpg',
+        'ttlink'=>'',
+        'name'=>'Sasha Meet',
+        'city'=>'Moskow'
+    )
+);
 ?>
-
 
 <pre style="height:100px;cursor:pointer" onclick="$(this).css({height:'inherit'})">
 <? print_r($arPromo); ?>
 </pre>
-
 
 <div class="row project">
 	<div class="col-xs-12">
@@ -45,19 +42,19 @@ $arPromo = array(
 </div>
 
 <div class="project__module">
-  <h1 class="project__title personal__title">ПЕРСОНАЛ</h1>
-  <div class="row">
+    <h1 class="project__title personal__title">ПЕРСОНАЛ</h1>
+    <div class="row">
     <? for($i = 0; $i < count($arPromo); $i ++): ?>
-    <div class="col-xs-12 col-sm-4 col-md-3">
-      <div class="personal__item">
-        <img src="<?=$arPromo[$i]['image']; ?>">
-        <div class="personal__item-name"><?=$arPromo[$i]['name']; ?></div>
-        <div class="personal__item-add">
-          <a href="<?=$arPromo[$i]['ttlink']; ?>">Закрепленные адреса</a>
+        <div class="col-xs-12 col-sm-4 col-md-3">
+              <div class="personal__item">
+                    <img src="<?=$arPromo[$i]['image']; ?>">
+                    <div class="personal__item-name"><?=$arPromo[$i]['name']; ?></div>
+                    <div class="personal__item-add">
+                        <a href="<?=$arPromo[$i]['ttlink']; ?>">Закрепленные адреса</a>
+                    </div>
+                    <div class="personal__item-city"><?=$arPromo[$i]['city']; ?></div>
+              </div>
         </div>
-        <div class="personal__item-city"><?=$arPromo[$i]['city']; ?></div>
-      </div>
-    </div>
     <? endfor;?>
     <!--<div class="col-xs-12 col-sm-4 col-md-3">-->
     <!--  <div class="personal__item">-->
@@ -89,5 +86,5 @@ $arPromo = array(
     <!--    <div class="personal__item-city">Рыбинск</div>-->
     <!--  </div>-->
     <!--</div>-->
-  </div>
+    </div>
 </div>
