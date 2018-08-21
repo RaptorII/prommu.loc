cb85ce74f4dbd09fc185a2898524069ee8192c72
Den <den@locahost>
2018-08-21 05:15:01 -0500

    Update project-staff.php

--- protected/views/frontend/user/projects/project-staff.php
+++ protected/views/frontend/user/projects/project-staff.php
@@ -1,7 +1,34 @@
 <?php
 Yii::app()->getClientScript()->registerCssFile($bUrl . '/theme/css/projects/item.css');
 Yii::app()->getClientScript()->registerScriptFile($bUrl . '/theme/js/projects/item.js', CClientScript::POS_END);
+$arPromo = array(
+                0 =>array(
+                    'image'=>'/images/applic/20180503073112204100.jpg', 
+                    'ttlink'=>'',
+                    'name'=>'Denis Gresko',
+                    'city'=>'Moskow'
+                ), 
+                1 =>array(
+                    'image'=>'/images/applic/20180503073112204100.jpg', 
+                    'ttlink'=>'',
+                    'name'=>'Denis Gresko',
+                    'city'=>'Moskow'
+                ), 
+                2 =>array(
+                    'image'=>'/images/applic/20180428142455264100.jpg', 
+                    'ttlink'=>'',
+                    'name'=>'Denis Gresko',
+                    'city'=>'Moskow'
+                ), 
+            );
+
 ?>
+
+
+<pre style="height:100px;cursor:pointer" onclick="$(this).css({height:'inherit'})">
+<? print_r($arPromo); ?>
+</pre>
+
 
 <div class="row project">
 	<div class="col-xs-12">
@@ -14,45 +41,47 @@ Yii::app()->getClientScript()->registerScriptFile($bUrl . '/theme/js/projects/it
 <div class="project__module">
   <h1 class="project__title personal__title">ПЕРСОНАЛ</h1>
   <div class="row">
+    <? for($i = 0; $i < count($arPromo); $i ++): ?>
     <div class="col-xs-12 col-sm-4 col-md-3">
       <div class="personal__item">
-        <img src="/images/applic/20180503160015590100.jpg">
-        <div class="personal__item-name">Шишкина Виктория</div>
+        <img src="<?=$arPromo[$i]['image']; ?>">
+        <div class="personal__item-name"><?=$arPromo[$i]['name']; ?></div>
         <div class="personal__item-add">
-          <a href="#">Закрепленные адреса</a>
+          <a href="<?=$arPromo[$i]['ttlink']; ?>">Закрепленные адреса</a>
         </div>
-        <div class="personal__item-city">Ростов</div>
+        <div class="personal__item-city"><?=$arPromo[$i]['city']; ?></div>
       </div>
     </div>
-    <div class="col-xs-12 col-sm-4 col-md-3">
-      <div class="personal__item">
-        <img src="/images/applic/20180503073112204100.jpg">
-        <div class="personal__item-name">Ибадулаев Павел</div>
-        <div class="personal__item-add">
-          <a href="#">Закрепленные адреса</a>
-        </div>
-        <div class="personal__item-city">Москва</div>
-      </div>
-    </div>
-    <div class="col-xs-12 col-sm-4 col-md-3">
-      <div class="personal__item">
-        <img src="/images/applic/20180430140946442100.jpg">
-        <div class="personal__item-name">Немич Константин</div>
-        <div class="personal__item-add">
-          <a href="#">Закрепленные адреса</a>
-        </div>
-        <div class="personal__item-city">Новгород</div>
-      </div>
-    </div>
-    <div class="col-xs-12 col-sm-4 col-md-3">
-      <div class="personal__item">
-        <img src="/images/applic/20180428142455264100.jpg">
-        <div class="personal__item-name">Простова Ольга</div>
-        <div class="personal__item-add">
-          <a href="#">Закрепленные адреса</a>
-        </div>
-        <div class="personal__item-city">Рыбинск</div>
-      </div>
-    </div>
+    <? endfor;?>
+    <!--<div class="col-xs-12 col-sm-4 col-md-3">-->
+    <!--  <div class="personal__item">-->
+    <!--    <img src="/images/applic/20180503073112204100.jpg">-->
+    <!--    <div class="personal__item-name">Ибадулаев Павел</div>-->
+    <!--    <div class="personal__item-add">-->
+    <!--      <a href="#">Закрепленные адреса</a>-->
+    <!--    </div>-->
+    <!--    <div class="personal__item-city">Москва</div>-->
+    <!--  </div>-->
+    <!--</div>-->
+    <!--<div class="col-xs-12 col-sm-4 col-md-3">-->
+    <!--  <div class="personal__item">-->
+    <!--    <img src="/images/applic/20180430140946442100.jpg">-->
+    <!--    <div class="personal__item-name">Немич Константин</div>-->
+    <!--    <div class="personal__item-add">-->
+    <!--      <a href="#">Закрепленные адреса</a>-->
+    <!--    </div>-->
+    <!--    <div class="personal__item-city">Новгород</div>-->
+    <!--  </div>-->
+    <!--</div>-->
+    <!--<div class="col-xs-12 col-sm-4 col-md-3">-->
+    <!--  <div class="personal__item">-->
+    <!--    <img src="/images/applic/20180428142455264100.jpg">-->
+    <!--    <div class="personal__item-name">Простова Ольга</div>-->
+    <!--    <div class="personal__item-add">-->
+    <!--      <a href="#">Закрепленные адреса</a>-->
+    <!--    </div>-->
+    <!--    <div class="personal__item-city">Рыбинск</div>-->
+    <!--  </div>-->
+    <!--</div>-->
   </div>
 </div>
