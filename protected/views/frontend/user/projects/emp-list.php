403d7a40e88bc46271a60c3514d5b5c22774de53
Dima Derevyanko <derevyanko977@gmail.com>
2018-08-23 17:32:53 +0300

    upgraded empl list of projects

--- protected/views/frontend/user/projects/emp-list.php
+++ protected/views/frontend/user/projects/emp-list.php
@@ -7,14 +7,21 @@
 ?>
 <div class="row projects">
 	<div class="col-xs-12">
-		<h1 class="projects__title">ВЫБЕРИТЕ ПРОЕКТ</h1>
-		<div class="projects__list">
-		    <? for($i = 0; $i < count($viData); $i ++):?>
-			<a href="/user/projects/<?=$viData[$i]['project']?>" class="projects__item"><?=$viData[$i]['name']?></a>
-			<? endfor;?>
-		</div>
-		<div class="projects__btn">
-			<a href="<?=MainConfig::$PAGE_PROJECT_NEW?>">+ Добавить проект</a>
-		</div>
+		<?php if(count($viData)): ?>
+			<h1 class="projects__title">ВЫБЕРИТЕ ПРОЕКТ</h1>
+			<div class="projects__list">
+				<?php for($i = 0; $i < count($viData); $i ++):?>
+					<a href="<? echo MainConfig::$PAGE_PROJECT_LIST . '/' . $viData[$i]['project'] ?>" class="projects__item"><?=$viData[$i]['name']?></a>
+				<?php endfor; ?>
+			</div>
+			<div class="projects__btn">
+				<a href="<?=MainConfig::$PAGE_PROJECT_NEW?>">+ Добавить проект</a>
+			</div>
+		<?php else: ?>
+			<h1 class="projects__title">У ВАС ПОКА НЕТ ПРОЕКТОВ</h1>
+			<div class="projects__btn empty">
+				<a href="<?=MainConfig::$PAGE_PROJECT_NEW?>">+ Добавить проект</a>
+			</div>
+		<?php endif; ?>
 	</div>
 </div>
