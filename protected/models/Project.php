c856a6f543042043abd4f77e2dca179ac8518819
Den <den@locahost>
2018-08-23 04:47:16 -0500

    Update Project.php

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -34,7 +34,7 @@ class Project extends ARModel
                         ));
     
         //*Обрабатываем входящие данные
-for($i = 0; $i < count($props['city']); $i++){
+            for($i = 0; $i < count($props['city']); $i++){
                 
                 $j = 0;
                 foreach($props['lindex'][$props['city'][$i]] as $key => $value){
@@ -110,20 +110,20 @@ for($i = 0; $i < count($props['city']); $i++){
                     $cloud[$s]['btime'] = $props['btime'][$props['city'][$i]][$j][$s];
                     $cloud[$s]['etime'] = $props['etime'][$props['city'][$i]][$j][$s];
 
-                    if($cloud[$s]['city']){
-                    $res = Yii::app()->db->createCommand()
-                        ->insert('project_city', array(
-                            'project' => $project,
-                            'title' => $title,
-                            'adres' => $cloud[$s]['lindex'],
-                            'name' => $cloud[$s]['lname'],
-                            'id_city' => $props['city'][$i],
-                            'bdate' => $cloud[$s]['bdate'],
-                            'edate' => $cloud[$s]['edate'],
-                            'btime' => $cloud[$s]['btime'],
-                            'etime' => $cloud[$s]['etime'],
-                        ));
-                    }
+                    // if($cloud[$s]['city']){
+                    // $res = Yii::app()->db->createCommand()
+                    //     ->insert('project_city', array(
+                    //         'project' => $project,
+                    //         'title' => $title,
+                    //         'adres' => $cloud[$s]['lindex'],
+                    //         'name' => $cloud[$s]['lname'],
+                    //         'id_city' => $props['city'][$i],
+                    //         'bdate' => $cloud[$s]['bdate'],
+                    //         'edate' => $cloud[$s]['edate'],
+                    //         'btime' => $cloud[$s]['btime'],
+                    //         'etime' => $cloud[$s]['etime'],
+                    //     ));
+                    // }
                 }
 
             }
@@ -131,35 +131,35 @@ for($i = 0; $i < count($props['city']); $i++){
 
         $users = explode(',', $props['users']);
 
-        for($i = 0; $i < count($users); $i ++){
-            if($users[$i]){
-            $res = Yii::app()->db->createCommand()
-                        ->insert('project_user', array(
-                            'project' => $project,
-                            'user' => $users[$i],
-                            'firstname' => 'firstname',
-                            'lastname' => 'lastname',
-                            'email' => 'email',
-                            'phone' => 'phone'
-                        ));
-                    }
-        }
+        // for($i = 0; $i < count($users); $i ++){
+        //     if($users[$i]){
+        //     $res = Yii::app()->db->createCommand()
+        //                 ->insert('project_user', array(
+        //                     'project' => $project,
+        //                     'user' => $users[$i],
+        //                     'firstname' => 'firstname',
+        //                     'lastname' => 'lastname',
+        //                     'email' => 'email',
+        //                     'phone' => 'phone'
+        //                 ));
+        //             }
+        // }
 
-        for($i = 0; $i < count($props['inv-name']); $i ++){
+        // for($i = 0; $i < count($props['inv-name']); $i ++){
           
-            $res = Yii::app()->db->createCommand()
-                        ->insert('project_user', array(
-                            'project' => $project,
-                            'user' => rand(1111,3334),
-                            'firstname' => $props['inv-name'][$i],
-                            'lastname' => $props['inv-sname'][$i],
-                            'email' => $props['inv-email'][$i],
-                            'phone' => $props['prfx-phone'][$i].$props['inv-phone'][$i],
-                        ));
+        //     $res = Yii::app()->db->createCommand()
+        //                 ->insert('project_user', array(
+        //                     'project' => $project,
+        //                     'user' => rand(1111,3334),
+        //                     'firstname' => $props['inv-name'][$i],
+        //                     'lastname' => $props['inv-sname'][$i],
+        //                     'email' => $props['inv-email'][$i],
+        //                     'phone' => $props['prfx-phone'][$i].$props['inv-phone'][$i],
+        //                 ));
                     
-        }
+        // }
 
-         return $project;
+         return $cloud;
     }
 
 
