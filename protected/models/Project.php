419dd3192977e783b7f644187f4b0b55c09acd9f
Den <den@locahost>
2018-08-29 00:24:40 -0500

    Update Project.php

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -48,6 +48,12 @@ class Project extends ARModel
                     $k++;
                 }
                 
+                $k = 0;
+                foreach($props['metro'][$props['city'][$i]] as $key => $value){
+                    $cloud['metro'][$props['city'][$i]][$k] = $value;
+                    $k++;
+                }
+                
                 $s = 0;
                 $j = 0;
                 foreach($props['bdate'][$props['city'][$i]] as $key => $value){
@@ -106,6 +112,7 @@ class Project extends ARModel
 
                     $clouds[$k]['city'] = $cloud['city'][$i];
                     $clouds[$k]['lindex'] = $cloud['lindex'][$cloud['city'][$i]][$j];
+                    $clouds[$k]['metro'] = $cloud['metro'][$cloud['city'][$i]][$j];
                     $clouds[$k]['lname'] = $cloud['lname'][$cloud['city'][$i]][$j];
                     $clouds[$k]['bdate'] = $cloud['bdate'][$cloud['city'][$i]][$j][$s];
                     $clouds[$k]['edate'] = $cloud['edate'][$cloud['city'][$i]][$j][$s];
