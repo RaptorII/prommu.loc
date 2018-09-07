850cf612f200c65e8103fdcb79d84e183664fd1f
Stanislav Kuzovov <s08002529@gmail.com>
2018-09-07 12:44:47 +0300

    Update Route

--- protected/views/frontend/user/projects/project-route.php
+++ protected/views/frontend/user/projects/project-route.php
@@ -11,91 +11,6 @@ Yii::app()->getClientScript()->registerScriptFile('https://code.jquery.com/ui/1.
 ?>
 
 
-
-
-<table class="route__table route__table-sort">
-    <thead>
-    <tr>
-        <th>№</th>
-        <th>Название ТТ</th>
-        <th>Адрес ТТ</th>
-        <th>Статус посещения</th>
-        <th>Дата</th>
-    </tr>
-    </thead>
-    <tbody id="sortable">
-    <tr class="ui-state-default">
-        <td>
-            1
-        </td>
-        <td>
-            <div class="route__table-cell border">АТБ1</div>
-        </td>
-        <td>
-            <div class="route__table-cell border route__table-index">
-                <span>ул. Пирогова 23</span>
-                <b class="js-g-hashint" title="Посмотреть на карте"></b>
-            </div>
-        </td>
-        <td>
-            <div class="route__table-cell border route__table-status">
-                <span>2</span>
-                <a href="#">изменить</a>
-            </div>
-        </td>
-        <td>
-            <div class="route__table-cell border text-center">14.02.2018</div>
-        </td>
-    </tr>
-    <tr class="ui-state-default">
-        <td>
-            2
-        </td>
-        <td>
-            <div class="route__table-cell border">ВАРУС</div>
-        </td>
-        <td>
-            <div class="route__table-cell border route__table-index">
-                <span>пр. Кирова 18</span>
-                <b class="js-g-hashint" title="Посмотреть на карте"></b>
-            </div>
-        </td>
-        <td>
-            <div class="route__table-cell border route__table-status">
-                <span>1</span>
-                <a href="#">изменить</a>
-            </div>
-        </td>
-        <td>
-            <div class="route__table-cell border text-center">14.02.2018</div>
-        </td>
-    </tr>
-    <tr class="ui-state-default">
-        <td>
-            3
-        </td>
-        <td>
-            <div class="route__table-cell border">СЕЛЬПО</div>
-        </td>
-        <td>
-            <div class="route__table-cell border route__table-index">
-                <span>ул. Строителей 4</span>
-                <b class="js-g-hashint" title="Посмотреть на карте"></b>
-            </div>
-        </td>
-        <td>
-            <div class="route__table-cell border route__table-status">
-                <span>3</span>
-                <a href="#">изменить</a>
-            </div>
-        </td>
-        <td>
-            <div class="route__table-cell border text-center">14.02.2018</div>
-        </td>
-    </tr>
-    </tbody>
-</table>
-
 <?
 $arFilterData = [
     'STYLES' => 'project__tasks-filter',
@@ -230,6 +145,7 @@ $arFilterData = [
     </div>
 
 
+
     <div class="routes">
         <div class="route__item">
             <h2 class="route__item-title">Харьков</h2>
@@ -323,5 +239,111 @@ $arFilterData = [
     <div class="routes__btns">
         <a href="#" class="route__watch-btn">ИЗМЕНИТЬ</a>
         <a href="#" class="route__watch-btn">СМОТРЕТЬ МАРШРУТ</a>
+    </div>
+</div>
+
+
+
+
+<div class="project__route-changer">
+    <div class="project__changer-content">
+        <table class="route__table route__table-sort">
+            <thead>
+            <tr>
+                <th>№</th>
+                <th>Название ТТ</th>
+                <th>Адрес ТТ</th>
+                <th>Статус посещения</th>
+                <th>Дата</th>
+            </tr>
+            </thead>
+            <tbody id="sortable">
+            <tr class="ui-state-default">
+                <td>
+                    1
+                </td>
+                <td>
+                    <div class="route__table-cell border">АТБ1</div>
+                </td>
+                <td>
+                    <div class="route__table-cell border route__table-index">
+                        <span>ул. Пирогова 23</span>
+                        <b class="js-g-hashint" title="Посмотреть на карте"></b>
+                    </div>
+                </td>
+                <td>
+                    <div class="route__table-cell border route__table-status">
+                        <span>2</span>
+                        <a href="#">изменить</a>
+                    </div>
+                </td>
+                <td>
+                    <div class="route__table-cell border text-center">14.02.2018</div>
+                </td>
+            </tr>
+            <tr class="ui-state-default">
+                <td>
+                    2
+                </td>
+                <td>
+                    <div class="route__table-cell border">ВАРУС</div>
+                </td>
+                <td>
+                    <div class="route__table-cell border route__table-index">
+                        <span>пр. Кирова 18</span>
+                        <b class="js-g-hashint" title="Посмотреть на карте"></b>
+                    </div>
+                </td>
+                <td>
+                    <div class="route__table-cell border route__table-status">
+                        <span>1</span>
+                        <a href="#">изменить</a>
+                    </div>
+                </td>
+                <td>
+                    <div class="route__table-cell border text-center">14.02.2018</div>
+                </td>
+            </tr>
+            <tr class="ui-state-default">
+                <td>
+                    3
+                </td>
+                <td>
+                    <div class="route__table-cell border">СЕЛЬПО</div>
+                </td>
+                <td>
+                    <div class="route__table-cell border route__table-index">
+                        <span>ул. Строителей 4</span>
+                        <b class="js-g-hashint" title="Посмотреть на карте"></b>
+                    </div>
+                </td>
+                <td>
+                    <div class="route__table-cell border route__table-status">
+                        <span>3</span>
+                        <a href="#">изменить</a>
+                    </div>
+                </td>
+                <td>
+                    <div class="route__table-cell border text-center">14.02.2018</div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="project__changer-buttons">
+
+        <div class="project__changer-button">
+            <img class="project__route-touch"
+                 src="http://pluspng.com/img-png/png-touch-feature-ultra-soft-texture-180.png"/>
+        </div>
+
+        <div class="project__changer-button">
+            <img class="project__route-touch touch__arrow-top"
+                 src="https://jobcart.ru/wp-content/uploads/2018/07/%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B0-%D0%B2%D0%B2%D0%B5%D1%80%D1%85.png"/>
+        </div>
+        <div class="project__changer-button">
+            <img class="project__route-touch touch__arrow-bottom"
+                 src="https://jobcart.ru/wp-content/uploads/2018/07/%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B0-%D0%B2%D0%B2%D0%B5%D1%80%D1%85.png"/>
+        </div>
     </div>
 </div>
