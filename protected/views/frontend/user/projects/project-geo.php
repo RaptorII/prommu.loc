e89f6f0eea919d63f1ac8b32aedd67ad3182e9db
Stanislav Kuzovov <s08002529@gmail.com>
2018-09-21 11:20:58 +0300

    Update project-geo.php  test

--- protected/views/frontend/user/projects/project-geo.php
+++ protected/views/frontend/user/projects/project-geo.php
@@ -133,6 +133,24 @@ $viData['states'] = array(
   2 => 'просрочил'
 );
 */ ?>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+      integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+      crossorigin=""/>
+
+<!-- Make sure you put this AFTER Leaflet's CSS -->
+<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+        integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+        crossorigin=""></script>
+
+<style>
+    #map { height: 400px; }
+</style>
+
+<div id="map"></div>
+<div id='actions'><a href='#'>Find me!</a></div>
+
+
+
 <pre style="height:100px;cursor:pointer" onclick="$(this).css({height:'inherit'})">
 <? print_r($viData); ?>
 </pre>
@@ -374,3 +392,26 @@ $viData['states'] = array(
         </div>
     <?php endif; ?>
 </div>
+
+
+<script>
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: ''
+    }).addTo(map);
+
+    function locate() {
+        map.locate({setView: true, maxZoom: 16});
+    }
+
+    function locate() {
+        map.locate({setView: true, maxZoom: 16});
+    }
+
+    $('#actions a').click(function(){
+        locate();
+    });
+
+
+</script>
