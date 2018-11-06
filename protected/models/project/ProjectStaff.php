895398d4d98815abf527012ed2d597f8a4371276
Den <den@locahost>
2018-11-06 04:51:29 -0600

    Update ProjectStaff.php

--- protected/models/project/ProjectStaff.php
+++ protected/models/project/ProjectStaff.php
@@ -415,8 +415,8 @@ class ProjectStaff extends CActiveRecordBehavior{
             //->order('pu.user desc')
             ->queryAll();
             
-         $main['mech'] = Yii::app()->db->createCommand()
-                    ->select("uad.name")
+         $main[] = Yii::app()->db->createCommand()
+                    ->select("uad.name as mech")
                     ->from('user_mech um')
                     ->leftjoin('user_attr_dict uad', 'uad.id=um.id_mech')
                     ->where(' um.isshow=0 AND um.id_us=:user_id', array(':user_id' => $user_id))
