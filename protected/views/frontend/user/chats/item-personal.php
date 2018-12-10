46ca5f0fe90107d4969fb367bb4401e2955b9f4c
Den <den@locahost>
2018-12-10 09:11:13 -0600

    Update item-personal.php

--- protected/views/frontend/user/chats/item-personal.php
+++ protected/views/frontend/user/chats/item-personal.php
@@ -1,19 +1,22 @@
 <?
-
-  $sectionList = MainConfig::$PAGE_CHATS_LIST_VACANCIES;
-  $this->setBreadcrumbsEx(array($viData['vacancy']['title'], $sectionList . DS . $vacancy . DS . $id));
-  $this->setPageTitle($viData['vacancy']['title']);
-
-  Yii::app()->getClientScript()->registerCssFile('/theme/css/chats/item.css');
-  Yii::app()->getClientScript()->registerCssFile('/jslib/magnific-popup/magnific-popup-min.css');
-  Yii::app()->getClientScript()->registerScriptFile('/theme/js/chats/item-emp.js', CClientScript::POS_END);
-  Yii::app()->getClientScript()->registerScriptFile('/jslib/nicedit/nicEdit.js', CClientScript::POS_END);
-  Yii::app()->getClientScript()->registerScriptFile('/jslib/magnific-popup/jquery.magnific-popup.min.js', CClientScript::POS_END);
+// echo "<pre>";
+// print_r($viData); 
+// echo "</pre>";
+	$sectionList = MainConfig::$PAGE_CHATS_LIST_VACANCIES;
+	$this->setBreadcrumbsEx(array($viData['vacancy']['title'], $sectionList . DS . $vacancy . DS . $id));
+	$this->setPageTitle($viData['vacancy']['title']);
+  $isNew = $viData['items']['new'];
+  $type = Share::$UserProfile->type;
+	Yii::app()->getClientScript()->registerCssFile('/theme/css/chats/item.css');
+	Yii::app()->getClientScript()->registerCssFile('/jslib/magnific-popup/magnific-popup-min.css');
+	Yii::app()->getClientScript()->registerScriptFile('/theme/js/chats/item-' . ($type==3?'emp.js':'app.js'), CClientScript::POS_END);
+	Yii::app()->getClientScript()->registerScriptFile('/jslib/nicedit/nicEdit.js', CClientScript::POS_END);
+	Yii::app()->getClientScript()->registerScriptFile('/jslib/magnific-popup/jquery.magnific-popup.min.js', CClientScript::POS_END);
 ?>
 <div class="message"><? echo ($viData['error'] ? $viData['message'] : '')?></div>
 <div class="chat-item">
-  <? if(!$viData['error']): ?>
-    <a href="<?=$sectionList?>" class="chat-item__btn-link"><span><</span> Назад</a>
+	<? if(!$viData['error']): ?>
+		<a href="<?=$sectionList?>" class="chat-item__btn-link"><span><</span> Назад</a>
       <?php if( $isNew ): ?>
         <div class="theme-input">
             <?php if( $viData['themes'] ): ?>
@@ -39,7 +42,7 @@
               <br />
               <br />
         </div>
-     <?php endif; ?>
+      <?php endif; ?>
 
       <div class='header-021'>
         <b>
@@ -149,7 +152,7 @@
 
       <script type="text/javascript">
       <!--
-         G_VARS.idTm = '<?=$viData['items'][0]['id_theme']?>';
+          G_VARS.idTm = '<?=$viData['items'][0]['id_theme']?>';
           <?php if( $isNew ): ?>
             G_VARS.isNew = '<?= $isNew ?>';
           <?php endif; ?>
@@ -187,5 +190,5 @@
               <span class="uni-delete js-hashint" title="удалить изображение"></span>
               <a href="" class="uni-link" target="_blank"></a>
       </div>
-  <?php endif; ?>
+	<?php endif; ?>
 </div>
