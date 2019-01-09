b6f445df0c9661641078f830c9020e1a3d3bc46b
Stanislav Kuzovov <s08002529@gmail.com>
2019-01-09 12:15:16 +0300

    update soiskatel tasks

--- protected/views/frontend/user/projects/applicant/tasks.php
+++ protected/views/frontend/user/projects/applicant/tasks.php
@@ -230,6 +230,7 @@ echo "</pre>";
             navigator.geolocation.getCurrentPosition(
                 function( position ){ // все в порядке
                     console.log( position );
+
                     console.log( position.coords.latitude );
                     console.log( position.coords.longitude );
                 },
