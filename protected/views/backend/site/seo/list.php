f6e2d75f677a6055f53ee4174c451f7ff5e6d2a9
Stanislav Kuzovov <s08002529@gmail.com>
2019-02-14 15:06:01 +0300

    update seo

--- protected/views/backend/site/seo/list.php
+++ protected/views/backend/site/seo/list.php
@@ -3,8 +3,138 @@
 .btn {
 	background: #00c0ef;
 }
+#tablist{
+    background-color: #222d32;
+    margin-bottom:30px;
+    text-align: justify;
+}
+#tablist li{
+    display: inline-block;
+}
+#tablist a{
+    font-weight: bold;
+    color: #b8c7ce;
+    border-left: 3px solid transparent;
+}
+#tablist .active a,
+#tablist a:hover{
+    color: #fff;
+    background: #1e282c;
+    border-left-color: #3c8dbc;
+}
 
+.tab-content .tab-pane{
+
+}
+
+.tab-content .tab-pane:nth-child(1){
+
+}
 </style>
+
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <ul class="nav user__menu" role="tablist" id="tablist">
+            <li class="active">
+                <a href="#seo" aria-controls="seo" role="tab" data-toggle="tab">Prommu Москва</a>
+            </li>
+            <li>
+                <a href="#seo_rostov" aria-controls="seo_rostov" role="tab" data-toggle="tab">Prommu Ростов</a>
+            </li>
+            <li>
+                <a href="#seo_spb" aria-controls="seo_spb" role="tab" data-toggle="tab">Prommu Санкт-Питербург</a>
+            </li>
+            <li>
+                <a href="#seo_novosibirsk" aria-controls="seo_novosibirsk" role="tab" data-toggle="tab">Prommu Новосибирск</a>
+            </li>
+            <li>
+                <a href="#seo_volgograd" aria-controls="seo_volgograd" role="tab" data-toggle="tab">Prommu Волгоград</a>
+            </li>
+            <li>
+                <a href="#seo_ufa" aria-controls="seo_ufa" role="tab" data-toggle="tab">Prommu Уфа</a>
+            </li>
+            <li>
+                <a href="#seo_voronezh" aria-controls="seo_voronezh" role="tab" data-toggle="tab">Prommu Воронеж</a>
+            </li>
+            <li>
+                <a href="#seo_krasnoyarsk" aria-controls="seo_krasnoyarsk" role="tab" data-toggle="tab">Prommu Красноярск</a>
+            </li>
+            <li>
+                <a href="#seo_omsk" aria-controls="seo_omsk" role="tab" data-toggle="tab">Prommu Омск</a>
+            </li>
+            <li>
+                <a href="#seo_perm" aria-controls="seo_perm" role="tab" data-toggle="tab">Prommu Пермь</a>
+            </li>
+            <li>
+                <a href="#seo_samara" aria-controls="seo_samara" role="tab" data-toggle="tab">Prommu Самара</a>
+            </li>
+            <li>
+                <a href="#seo_ekaterinburg" aria-controls="seo_ekaterinburg" role="tab" data-toggle="tab">Prommu Екатеринбург</a>
+            </li>
+            <li>
+                <a href="#seo_kazan" aria-controls="seo_kazan" role="tab" data-toggle="tab">Prommu Казань</a>
+            </li>
+            <li>
+                <a href="#seo_chelyabinsk" aria-controls="seo_chelyabinsk" role="tab" data-toggle="tab">Prommu Челябинск</a>
+            </li>
+            <li>
+                <a href="#seo_nn" aria-controls="seo_nn" role="tab" data-toggle="tab">Prommu Нижний Новгород</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane fade active in" id="seo">
+            seo mos
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_ufa">
+            seo_ufa
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_volgograd">
+            seo_volgograd
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_voronezh">
+            seo_voronezh
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_nn">
+            seo_nn
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_krasnoyarsk">
+            seo_krasnoyarsk
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_novosibirsk">
+            seo_novosibirsk
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_omsk">
+            seo_omsk
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_perm">
+            seo_perm
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_rostov">
+            seo_rostov
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_samara">
+            seo_samara
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_spb">
+            seo_spb
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_ekaterinburg">
+            seo_ekaterinburg
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_kazan">
+            seo_kazan
+        </div>
+        <div role="tabpanel" class="tab-pane fade in" id="seo_chelyabinsk">
+            seo_chelyabinsk
+        </div>
+    </div>
+</div>
+
 <a href="<?php echo Yii::app()->createUrl('site/seoAdd'); ?>" class="btn" id="btn_add">Добавить URL</a>
 <div class="modal hide fade" id="Modal" tabindex="-1" role="dialog">
 	<div class="modal-header">
@@ -38,7 +168,7 @@
 		'id'=>'my-grid',
 		'dataProvider'=>$model->search(),
 		'filter'=>$model,
-		 'itemsCssClass' => 'table table-bordered table-hover dataTable',
+		 'itemsCssClass' => 'table table-bordered table-hover dataTable col-xs-12 col-sm-9 col-md-10',
     'htmlOptions'=>array('class'=>'table table-hover', 'name'=>'my-grid', 'style'=>    'padding: 10px;'),
 		'columns'=>array
 		(
