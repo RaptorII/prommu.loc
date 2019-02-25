c480b41680258a78fc81042a911f732ce7f1be59
Den <den@locahost>
2019-02-25 15:44:12 -0600

    Update page-login-tpl.php

--- protected/views/frontend/user/page-login-tpl.php
+++ protected/views/frontend/user/page-login-tpl.php
@@ -66,6 +66,7 @@
           <a href="/user/login?service=mailru" class="auth-form__social-link ml js-g-hashint" title="mail.ru"><span class="mob-hidden">mail.ru</a>
           <a href="/user/login?service=odnoklassniki" class="auth-form__social-link od js-g-hashint" title="odnoklasniki.ru"><span class="mob-hidden">odnoklasniki.ru</span></a>
           <a href="/user/login?service=google_oauth" class="auth-form__social-link go js-g-hashint" title="google+"><span class="mob-hidden">google+</span></a>
+          <a href="/user/login?service=yandex_oauth" class="auth-form__social-link ya js-g-hashint tooltipstered"><span class="mob-hidden">yandex</span></a>
       </div>
     </div>
   </div>
