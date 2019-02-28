df30cd4927b4f98d5ac36403662238d3372bb030
Den <den@locahost>
2019-02-28 12:28:11 -0600

    Update YandexOAuthService.php

--- protected/extensions/eauth/services/YandexOAuthService.php
+++ protected/extensions/eauth/services/YandexOAuthService.php
@@ -37,9 +37,9 @@ class YandexOAuthService extends EOAuth2Service {
 
 		$this->attributes['id'] = $info['id'];
 		$this->attributes['name'] = $info['real_name'];
-		//$this->attributes['login'] = $info['display_name'];
-		//$this->attributes['email'] = $info['emails'][0];
-		//$this->attributes['email'] = $info['default_email'];
+		$this->attributes['login'] = $info['display_name'];
+		$this->attributes['email'] = $info['emails'][0];
+		$this->attributes['email'] = $info['default_email'];
 		$this->attributes['gender'] = ($info['sex'] == 'male') ? 'M' : 'F';
 	}
 
