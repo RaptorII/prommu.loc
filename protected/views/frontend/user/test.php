04afec70ca7ee47f3f42a072893d6fc5c33b4323
Den <den@locahost>
2019-03-21 15:50:43 -0500

    Update test.php

--- protected/views/frontend/user/test.php
+++ protected/views/frontend/user/test.php
@@ -120,130 +120,14 @@ $arUser = $arUser[$userId];
 </style>
 
 <div class="user__rating">
-    <div class="user__info">
-
-
-        <div class="user__info-part1">
-            <img src="/images/applic/20180416165041951400.jpg" alt="" class="user__info-logo"/>
-        </div>
-        <div class="user__info-part2">
-            <h2 class="user__info-title">Dmitry Derevyanko</h2>
-
-            <div class="">
-
-                <span>ОБЩИЙ РЕЙТИНГ  </span><b>31.7</b> ИЗ 100 БАЛЛОВ
-
-            </div>
-            <hr class="user__rating-line"/>
-            <table class="upp__table">
-                <tbody>
-                <tr>
-                    <td class="upp__table-name">
-                        <span>Качество выполненной работы</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-plus js-g-hashint tooltipstered">1</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-zero js-g-hashint tooltipstered">0</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-minus js-g-hashint tooltipstered">0</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="upp__table-name">
-                        <span>Контактность</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-plus js-g-hashint tooltipstered">0</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-zero js-g-hashint tooltipstered">0</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-minus js-g-hashint tooltipstered">0</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="upp__table-name">
-                        <span>Пунктуальность</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-plus js-g-hashint tooltipstered">0</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-zero js-g-hashint tooltipstered">0</span>
-                    </td>
-                    <td class="upp__table-cnt">
-                        <span class="upp__table-cnt-minus js-g-hashint tooltipstered">1</span>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
-    </div>
+    
 
 
     <h2>Рейтинг:</h2>
-    <div class="user__diagram" id="oil" style="width: 100%; height: 400px;">
-
-    
-
-    </div>
-
-    <div class="user__reviews">
-        <h2>Отзывы:</h2>
-        <div class="user__reviews-comments">
-            <div class="user__reviews-item good">
-                <div class="user__reviews-name"><a href="/ankety/7000">Дмитрий Деревянко 100</a> 14/01/2019</div>
-                <div class="user__reviews-text">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
-                    quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-                    imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
-                    Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-                    porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis,
-                    feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-                    imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam
-                    rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet
-                    adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-                    Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam
-                    quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet
-                    nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus
-                    nunc,
-                </div>
-            </div>
-
-            <div class="user__reviews-item bad">
-                <div class="user__reviews-name"><a href="/ankety/7000">Дмитрий Деревянко 100</a> 14/01/2019</div>
-                <div class="user__reviews-text">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
-                    quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-                    imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
-                    Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-                    porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis,
-                    feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-                    imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam
-                    rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet
-                    adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-                    Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam
-                    quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet
-                    nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus
-                    nunc,
-                </div>
-            </div>
-
-        </div>
-
-
-    </div>
+   <div class="user__diagram" id="oil" style="width: 100%; height: 400px;">
 </div>
 <?
+var_dump($viData);
                 $ratingJson = [];
                 $ratingCount = 0;
                 $ratingJson[$ratingCount] = ['Дата', 'хорошо', 'плохо'];
