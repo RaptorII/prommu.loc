3adbfbc547e5bed2dd4d22b0da5429835d112e4a
Den <den@locahost>
2019-03-28 06:01:54 -0500

    Update Api.php

--- protected/models/Api.php
+++ protected/models/Api.php
@@ -117,8 +117,8 @@ class Api
     
     public function getFaq(){
         $type = Yii::app()->getRequest()->getParam('type');
-        if($type == '2') $type = 1;
-        if($type == '3') $type = 2;
+        if($type == 2) $type = 1;
+        if($type == 3) $type = 2;
         
         $faq = new Faq();
         $res = $faq->getFaqAll($type);
