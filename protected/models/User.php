451346671588af0c99ecee94653c7cc245e09ef4
Den <den@locahost>
2019-05-08 06:51:11 -0500

    Update User.php

--- protected/models/User.php
+++ protected/models/User.php
@@ -334,7 +334,51 @@ class User extends CActiveRecord
     			}
 				
 			}
+            
+        $body = $data['bodyAttribs'];
+        Yii::app()->db->createCommand()->delete('user_attribs', 'id_us=:id_user', array(':id_user' => $id));
+        
+        foreach($body as $key=>$val) {
+			    $result = Yii::app()->db->createCommand()
+    			->select('*')
+    			->from('user_attribs')
+                ->where("id_us=:id_user and `key`=:key", array(':id_user'=>$id, ':key'=>$key))
+    			->queryRow();
+    			
+    			if($result['key'] == $key){
+    			    
+    			    $userdict = Yii::app()->db->createCommand()
+                        ->select('d.id , d.type, d.key')
+                        ->from('user_attr_dict d')
+                        ->where('d.id = :key', array(':key' => $val))
+                        ->queryRow();
+                        
+    			    Yii::app()->db->createCommand()
+					->update('user_attribs', array(
+						'val' => $userdict['name'],
+					), "id_us=:id_user and `key`=:key", array(':id_user' => $id, ':key' => $key));
+				
+    			} else {
 
+            		$userdict = Yii::app()->db->createCommand()
+                        ->select('d.id , d.type, d.key')
+                        ->from('user_attr_dict d')
+                        ->where('d.id = :key', array(':key' => $val))
+                        ->queryRow();
+
+            			
+    			    Yii::app()->db->createCommand()
+                    ->insert('user_attribs', array(
+                        'id_attr' => $userdict['id'],
+                        'type' => $userdict['type'],
+                        'val' => $val,
+                        'key' => $key,
+                        'id_us' => $id,
+                        'crdate' => date("Y-m-d H:i:s")
+                    ));
+    			}
+				
+			}
 			
         return array('error'=>0,  'message'=>'success' ,'sendmail'=>0); 
 	}
