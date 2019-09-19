60338166af5ec131cbcd141ec296135e2f91b95a
Mike <mihail@prommu.com>
2019-09-19 11:10:10 +0300

    fix reg views

--- protected/views/frontend/user/page-login-tpl.php
+++ protected/views/frontend/user/page-login-tpl.php
@@ -8,7 +8,7 @@
 <div class='row'>
   <div class='col-xs-12 auth-form__wrap'>
     <div class="row auth-form__header">
-        <div class="col-xs-12 col-sm-8"><h1 class='auth-form__title'>Вход на сайт</h1></div>
+        <div class="col-xs-12 col-sm-8"><h1 class='auth-form__title'>Вход</h1></div>
         <div class="col-xs-12 col-sm-4"></div>
     </div>
 
