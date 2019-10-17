79c8351cf99c528d5c928a8adc071d7bddfc256f
Mike <mihail@prommu.com>
2019-10-17 10:31:24 +0300

    add error to reg-forms. class login__error

--- protected/views/frontend/user/register/step_2.php
+++ protected/views/frontend/user/register/step_2.php
@@ -1,6 +1,14 @@
 <?php if (count($viData['errors'])): ?>
   <div class="danger">- <?= implode('<br>- ', $viData['errors']); ?></div>
-<?php endif; ?>
+
+<?php
+
+    endif;
+
+//    echo'<pre>';
+//    print_r ($viData);
+//    echo'</pre>';
+?>
     <form id="register_form">
 
         <div class="login-wrap">
@@ -14,17 +22,53 @@
                 <?php if (Share::isApplicant($viData['input']['type'])): ?>
                   <p>
                     <input type="text" name="name" value="<?=$viData['input']['name']?>" class="input-name" autocomplete="off" placeholder="Имя">
+                    <?php
+                      if (!$viData['error']) {
+                          ?>
+                          <span class="login__error">
+                                <?='Ой лишенько! Трапилась халепа! Ви ввели неправильне ім\'я або некорректні символи.';?>
+                            </span>
+                          <?php
+                      }
+                    ?>
                   </p>
                   <p>
                     <input type="text" name="surname" value="<?=$viData['input']['surname']?>" class="input-surname" autocomplete="off" placeholder="Фамилия">
+                    <?php
+                      if (!$viData['error']) {
+                          ?>
+                          <span class="login__error">
+                                <?='Ой лишенько! Трапилась халепа! Ви ввели неправильне ім\'я або некорректні символи.';?>
+                            </span>
+                          <?php
+                      }
+                    ?>
                   </p>
                 <?php elseif (Share::isEmployer($viData['input']['type'])): ?>
                   <p>
                     <input type="text" name="name" value="<?=$viData['input']['name']?>" class="input-company" autocomplete="off" placeholder="Название компании">
+                      <?php
+                        if (!$viData['error']) {
+                            ?>
+                            <span class="login__error">
+                                <?='Ой лишенько! Трапилась халепа! Ви ввели неправильне ім\'я або некорректні символи.';?>
+                            </span>
+                            <?php
+                        }
+                      ?>
                   </p>
                 <?php endif; ?>
                 <p>
                     <input type="text" name="login" value="<?=$viData['input']['login']?>" class="input-login" autocomplete="off" placeholder="Телефон или e-mail">
+                    <?php
+                      if (!$viData['error']) {
+                        ?>
+                        <span class="login__error">
+                                <?='Ой лишенько! Трапилась халепа! Ви ввели неправильне ім\'я або некорректні символи.';?>
+                            </span>
+                        <?php
+                      }
+                    ?>
                 </p>
 
                 <p class="input">
