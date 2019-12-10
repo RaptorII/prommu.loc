f3ef81e64ae41c4d177157c3608dc7f3babec908
Den <den@locahost>
2019-12-10 03:57:34 -0600

    Update ProfileFactory.php

--- protected/models/factory/ProfileFactory.php
+++ protected/models/factory/ProfileFactory.php
@@ -26,18 +26,19 @@ class ProfileFactory
             if( Share::$UserProfile instanceof UserProfile )
                 if( $props->id == Share::$UserProfile->id ) return Share::$UserProfile;
 
-                    
+                     
             // если тип не определен
 //            if( !isset($props->type) )
             if( !$props->type )
             {
                 list($props->idProfile, $props->type) = array_values(UserProfile::getUserType($props->id));
-                 var_dump(array_values(UserProfile::getUserType($props->id)));
+                
 //                $res = UserProfile::getUserType($props->id);
 //                $props->idProfile = $res['id'];
 //                $props->type = $res['type'];
             } // endif
-
+        
+            var_dump($props->type);
             if( $props->type == 2 ) $Profile = new UserProfileApplic($props);
             elseif( $props->type == 3 ) $Profile = new UserProfileEmpl($props);
             else $Profile = (object)array('error' => -101, 'message' => 'Нет данных для такого пользователя');
