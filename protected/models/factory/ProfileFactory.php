53761c49fd00fc22746941f5f8687bdd13be2ecc
Den <den@locahost>
2019-12-10 03:53:14 -0600

    Update ProfileFactory.php

--- protected/models/factory/ProfileFactory.php
+++ protected/models/factory/ProfileFactory.php
@@ -8,50 +8,44 @@
 
 class ProfileFactory
 {
-  /**
+//    protected $id; // id user
+
+
+
+    /**
      * @param $inProps array (id, type)
      * @return UserProfile
      */
     public function makeProfile($inProps)
     {
-      $props = (object)$inProps;
+        $props = (object)$inProps;
 
-      if( $props->id )
-      {
-        // если id текущего пользователя - его и возвращаем
-        if(
-          Share::$UserProfile instanceof UserProfile
-          &&
-          $props->id==Share::$UserProfile->id
-        )
+        if( $props->id )
         {
-          return Share::$UserProfile;
-        }
+            // если id текущего пользователя - его и возвращаем
+            if( Share::$UserProfile instanceof UserProfile )
+                if( $props->id == Share::$UserProfile->id ) return Share::$UserProfile;
 
-        // если тип не определен
-        if( !$props->type )
-        {
-          list($props->idProfile, $props->type) = array_values(UserProfile::getUserType($props->id));
-        }
 
-        if( Share::isApplicant($props->type) )
-        {
-          $Profile = new UserProfileApplic($props);
-        }
-        elseif( Share::isEmployer($props->type) )
-        {
-          $Profile = new UserProfileEmpl($props);
+            // если тип не определен
+//            if( !isset($props->type) )
+            if( !$props->type )
+            {
+                list($props->idProfile, $props->type) = array_values(UserProfile::getUserType($props->id));
+//                $res = UserProfile::getUserType($props->id);
+//                $props->idProfile = $res['id'];
+//                $props->type = $res['type'];
+            } // endif
+
+            if( $props->type == 2 ) $Profile = new UserProfileApplic($props);
+            elseif( $props->type == 3 ) $Profile = new UserProfileEmpl($props);
+            else $Profile = (object)array('error' => -101, 'message' => 'Нет данных для такого пользователя');
+
+            return $Profile;
         }
         else
         {
-          $Profile = (object)['error'=>-101, 'message'=>'Нет данных для такого пользователя'];
-        }
-
-        return $Profile;
-      }
-      else
-      {
-        return new UserProfileGuest(['id'=>0, 'type'=>0]);
-      }
+            return new UserProfileGuest(array('id' => 0, 'type' => 0));
+        } // endif
     }
 }
