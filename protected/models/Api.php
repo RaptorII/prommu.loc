371422ac98f077ac939107b182a4cbdcb9f133c8
Den <den@locahost>
2019-12-21 14:18:32 -0600

    Update Api.php

--- protected/models/Api.php
+++ protected/models/Api.php
@@ -3843,10 +3843,12 @@ public function vac(){
     
         // получаем данные страницы
         $SearchEmpl = new SearchEmpl();
+        
         $pages = new CPagination($SearchEmpl->searchEmployersCount($filter));
         $pages->pageSize = 50;
         $pages->applyLimit($SearchEmpl);
         
+        var_dump($filter);
         // отсеивать из ответа работодателей
         $data = array_values($SearchEmpl->getEmployersAPI(0, $filter)['empls']);
         $data = array_merge(['employers' => $data, 'pageCount' => $pages->pageCount,'currentPage' => (int)$page ? (int)$page : 1]);
