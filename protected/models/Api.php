a27d8408182227cbd388fe35f207ffdb6bcd25f8
Den <den@locahost>
2020-01-29 04:06:56 -0600

    Update Api.php

--- protected/models/Api.php
+++ protected/models/Api.php
@@ -3367,9 +3367,9 @@ public function vac(){
         $id = $idus;
         $current =  base64_decode($photo);
             
-        $res = mkdir("/var/www/files_prommu/test/".$id, 0777);
+        $res = mkdir("/var/www/files_prommu/test/".$id,  0700);
         echo $res;
-        $res = mkdir("/var/www/files_prommu/test/".$id."/tmp/", 0777);
+        $res = mkdir("/var/www/files_prommu/test/".$id."/tmp/",  0700);
         echo $res;
         
         $name = date('YmdHis').rand(100,1000);
