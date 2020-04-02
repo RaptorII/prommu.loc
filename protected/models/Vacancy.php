896468f7919e1e169ffeb3c0d470fbf1d4bbd0f9
Den <den@locahost>
2020-04-02 10:13:44 -0500

    Update Vacancy.php

--- protected/models/Vacancy.php
+++ protected/models/Vacancy.php
@@ -791,7 +791,6 @@ class Vacancy extends ARModel
             LEFT JOIN empl_city c1 ON c1.id_vac = e.id 
             LEFT JOIN city c2 ON c2.id_city = c1.id_city 
             JOIN empl_attribs ea ON ea.id_vac = e.id
-            JOIN user_attr_dict d ON (d.id = ea.id_attr) AND (d.id_par = 110)
             JOIN employer em ON em.id_user = e.id_user
             JOIN user u ON u.id_user = em.id_user
             WHERE e.id_user= {$inId}
@@ -799,7 +798,7 @@ class Vacancy extends ARModel
             $res = Yii::app()->db->createCommand($sql);
             $res = $res->queryAll();
                 
-            var_dump($res);
+            //JOIN user_attr_dict d ON (d.id = ea.id_attr) AND (d.id_par = 110)
              // AND (d.id_par = 110)
        
         foreach ($res as $key => $val)
