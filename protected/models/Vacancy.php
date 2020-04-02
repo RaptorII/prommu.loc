7922c26761f3f094d02679d1a5c427fa47426e3d
Den <den@locahost>
2020-04-02 10:16:43 -0500

    Update Vacancy.php

--- protected/models/Vacancy.php
+++ protected/models/Vacancy.php
@@ -791,6 +791,7 @@ class Vacancy extends ARModel
             LEFT JOIN empl_city c1 ON c1.id_vac = e.id 
             LEFT JOIN city c2 ON c2.id_city = c1.id_city 
             JOIN empl_attribs ea ON ea.id_vac = e.id
+            JOIN user_attr_dict d ON (d.id = ea.id_attr) AND (d.id_par = 110)
             JOIN employer em ON em.id_user = e.id_user
             JOIN user u ON u.id_user = em.id_user
             WHERE e.id_user= {$inId}
